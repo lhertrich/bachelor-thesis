be050eb0ab52568f79ebbb8b5febe27839ece562
Levin <st166200@stud.uni-stuttgart.de>
2023-10-26 14:34:35 +0200

    implemented chromodb

--- view_data_preparation.py
+++ view_data_preparation.py
@@ -7,10 +7,23 @@ def reduce_view_data_create_view():
     df = read_file()
     df['ObjectSQLCode_First_32767_Characters'] = df['ObjectSQLCode_First_32767_Characters'].str.replace(r'^.*?create view', 'create view', case=False, regex=True)
     df.to_csv('View_Data_createView.csv', sep=';', index=False)
+
+def clean_content(content):
+    # Remove /* and */
+    #content = content.replace("/*", "").replace("*/", "")
+    # Remove sequences of =- and -=
+    content = content.replace("=-", "").replace("-=", "")
+    # Remove sequences of ==
+    content = content.replace("==", "")
+    # Remove sequences of multiple spaces with a single space
+    content = " ".join(content.split())
+    return content
     
 def reduce_view_data_information():
     df = read_file()
     df['ObjectSQLCode_First_32767_Characters'] = df['ObjectSQLCode_First_32767_Characters'].str.replace(r'create view.*$', '', case=False, regex=True)
+    df['ObjectSQLCode_First_32767_Characters'] = df['ObjectSQLCode_First_32767_Characters'].apply(clean_content)
     df.to_csv('View_Data_Information.csv', sep=';', index=False)
 
 reduce_view_data_information()
+
